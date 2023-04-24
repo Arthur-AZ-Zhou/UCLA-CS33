e2ab8a756b3e3f7ca546c7ce0853a030cc184d44
Arthur Zhou <arthuracct@gmail.com>
2023-04-23 22:14:47 -0700

    Lab 1 Done - Solved Data Lab, a series of integer functions, using only bitwise operators. See README for more details. In bits.c file

---
+++ Lab1/datalab-handout/bits.c
@@ -0,0 +1,309 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * Arthur Zhou
+ * 405999589
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting if the shift amount
+     is less than 0 or greater than 31.
+
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+
+NOTES:
+  1. Our checker requires that you do NOT define a variable after 
+     a statement that does not define a variable.
+
+     For example, this is NOT allowed:
+
+     int illegal_function_for_this_lab(int x, int y) {
+      // this statement doesn't define a variable
+      x = x + y + 1;
+      
+      // The checker for this lab does NOT allow the following statement,
+      // because this variable definition comes after a statement 
+      // that doesn't define a variable
+      int z;
+
+      return 0;
+     }
+     
+  2. VERY IMPORTANT: Use the dlc (data lab checker) compiler (described in the handout)
+     to check the legality of your solutions.
+  3. Each function has a maximum number of operations (integer, logical,
+     or comparison) that you are allowed to use for your implementation
+     of the function.  The max operator count is checked by dlc.
+     Note that assignment ('=') is not counted; you may use as many of
+     these as you want without penalty.
+  4. Use the btest to check your functions for correctness.
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. 
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the btest to verify that your solutions produce 
+ *      the correct answers.
+ */
+
+
+#endif
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+//1
+/*
+ * isTmax - returns 1 if x is the maximum, two's complement number,
+ *     and 0 otherwise 
+ *   Legal ops: ! ~ & ^ | +
+ *   Max ops: 10
+ *   Rating: 1
+ */
+int isTmax(int x) {
+    /*first add 1 to x to make sure there's no possibility of overflow. x += 1 should net Tmin if x is Tmax, then add Tmin to get 0 if x is Tmax*/
+    x = x + 1; //should get Tmin if x is Tmax
+    x = x + 0x80000000;
+    return !x;
+}
+//2
+/* 
+ * evenBits - return word with all even-numbered bits set to 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int evenBits(void) {
+    /*just return the binary representation of all even bits set to 1*/
+    int word = 0x55;
+    word = word | word << 8;
+    word = word | word << 16;
+    return word;
+}
+//3
+/* 
+ * isEqual - return 1 if x == y, and 0 otherwise 
+ *   Examples: isEqual(5,5) = 1, isEqual(4,5) = 0
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int isEqual(int x, int y) {
+    return !(x^y);
+}
+//4
+/* 
+ * fitsBits - return 1 if x can be represented as an 
+ *  n-bit, two's complement integer.
+ *   1 <= n <= 32
+ *   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int fitsBits(int x, int n) {
+    /*find the correct value to shift by, aka 32 - n and then shift it left and right ot see if anything changed*/
+    int shiftVal = 33 + ~n; //equivalent to 32 - n bc you can't use - 
+    int newX = (x << shiftVal) >> shiftVal; //arithmetic
+    return !(x^newX);
+}
+//5
+/* 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+    /*if x is nonzero, the mask gives us 0xffffffff, if x is zero we get 0x00000000. This works perfectly because if x is nonzero we mask z because
+    ~mask gives 0x00000000 and &-ing it with z gives you 0. If x is zero we mask y because mask will be 0x00000000 and &-ing it with y gives 0*/
+    int mask = (!x + ~0x00); //if x is nonzero, we get 1111..., if x is zero, we get 0000...
+    return ((mask) & y) | ((~mask) & z); //if x nonzero, mask z. If x = 0, mask y
+}
+//6
+/* 
+ * isGreater - if x > y  then return 1, else return 0 
+ *   Example: isGreater(4,5) = 0, isGreater(5,4) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isGreater(int x, int y) {
+    /*xSign and ySign return 0xff if negative and 0x00 if positive. If they are same sign check for which one is greater. Otherwise if they're opposite
+    signs return 0x00 if x is positive and y is negative, return 0xff vice versa*/
+    int xSign = x >> 31;
+    int ySign = y >> 31;
+    int sameSign = ((!(xSign^ySign)) & ((x + ~y) >> 31));
+    int oppSign = xSign & !(ySign);
+    return !(sameSign | oppSign);
+}
+//7
+/*
+ * multFiveEighths - multiplies by 5/8 rounding toward 0.
+ *   Should exactly duplicate effect of C expression (x*5/8),
+ *   including overflow behavior.
+ *   Examples: multFiveEighths(77) = 48
+ *             multFiveEighths(-22) = -13
+ *             multFiveEighths(1073741824) = 13421728 (overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 3
+ */
+int multFiveEighths(int x) {
+    /*similar concept to ez3/4ths, add a bias of eight - 1 to help with negative numbers since negative numbers always round towards negative infinity*/
+    int fiveX = x + x + x + x + x;
+    int bias = (fiveX >> 31) & 7;
+    return ((fiveX + bias) >> 3);
+}
+//8
+/* 
+ * logicalNeg - implement the ! operator, using all of 
+ *              the legal operators except !
+ *   Examples: logicalNeg(3) = 0, logicalNeg(0) = 1
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 4 
+ */
+int logicalNeg(int x) {
+    /*expression gets you 0x00 if x is 0, and 0xff... otherwise. Then the return returns 1 if expression is nonzero and 0 if expression is 0*/
+    int expression = (x | (~x + 1)) >> 31;
+    return ~expression & 0x01;
+}
+//9
+/* 
+ * twosComp2SignMag - Convert from two's complement to sign-magnitude 
+ *   where the MSB is the sign bit
+ *   You can assume that x > TMin
+ *   Example: twosComp2SignMag(-5) = 0x80000005.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 4
+ */
+int twosComp2SignMag(int x) {
+    int xSign = x >> 31;
+    int signExtend = xSign << 31;
+    int ignoreSign = ~x + 1; //ignoring sign gives 0xff... if x is positive
+    return (~xSign & x) | (xSign & (signExtend | ignoreSign));
+}
+//10
+/*
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int isPower2(int x) {
+    /*if it's negative you return 0 immediately, otherwise x & (x - 1) should be 0x00 and as long as x isn't 0 it works*/
+    int isNeg = x >> 31; //gives 0xff... if negative
+    return !isNeg & !!x & !(x & (x + ~0x00));
+}
